2265a3644052aa88af6628a67bd429714f2da3e3
furkansari <furkansari@3eendustriyel.com.tr>
2024-04-24 16:33:36 +0300

    .

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+using MeetingApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MettingApp.Controllers
+{
+    public class HomeController : Controller{
+
+        public IActionResult Index()
+        {
+            int saat = DateTime.Now.Hour;
+
+            //var selamlama = saat > 12 ? "İyi Günler" : "Günaydın";
+            //return View(model: selamlama);
+
+            //ViewBag
+            //ViewBag.Selamlama = saat > 12 ? "İyi Günler" : "Günaydın";
+            //ViewBag.UserName = "Furkan";
+            //cshtml: @ViewBag.Selamlama, @ViewBag.UserName
+
+            //ViewData
+            ViewData["Selamlama"] = saat > 12 ? "İyi Günler" : "Günaydın";
+            ViewData["UserName"] = "Furkan";
+            //cshmtl: @ViewData["Selamlama"], @ViewData["UserName"]
+
+            var meetingInfo = new MeetingInfo(){
+                Id = 1,
+                Location = "Istanbul, ABC Merkezi",
+                Date = new DateTime(2024, 01, 20, 20, 00, 00),
+                NumberOfPeople = 100
+            };
+
+            return View(meetingInfo);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,37 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); //For mvc pattern
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//wwwroot içindeki static dosyalar
+app.UseStaticFiles();
+
+/* //wwwroot dışında başka bir path deki static dosyaları kullanmak için
+using Microsoft.Extensions.FileProviders;
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "MyStaticFiles")),
+    RequestPath = "/StaticFiles"
+});*/
+
+
+app.UseRouting();
+
+//mvc
+//rest api
+//razor pages
+
+// {controller = home}/{action = index}/id?
+//app.MapDefaultControllerRoute();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
+
+
 
 app.Run();
